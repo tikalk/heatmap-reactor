660df4d973ab2c0a1891ea67676d26896f83fc25
Yanai Franchi <yanai@yanai-laptop.home>
2015-05-25 22:49:36 +0300

    change

--- checkinsHttpServer.js
+++ checkinsHttpServer.js
@@ -3,6 +3,8 @@ var container = require('vertx/container');
 var console = require('vertx/console');
 var config = container.config;
 
+container.deployModule("com.zanox.vertx~mod-kafka~1.1.0", config);
+
 vertx.createHttpServer().requestHandler(function(request) {
 	request.dataHandler(function(buffer) {
    		vertx.eventBus.send(config.address, {payload:buffer.toString()});
